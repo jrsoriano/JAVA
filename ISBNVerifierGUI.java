5c4c4a0377971df1c4c8623b668f4d9cf0518a80
Jose R. Soriano <jrsoriano@users.noreply.github.com>
2017-07-13 10:29:22 -0500

    Update ISBNVerifierGUI.java

--- ISBNVerifierGUI.java
+++ ISBNVerifierGUI.java
@@ -1,15 +1,13 @@
 /**
- * Assignment 3 Creating Exceptions
+ * 
  * This application will validate ISBN-10 number entered into a custom TextField object and throw custom exceptions if ISBN-10 is invalid.
  * 
  * @author Jose R. Soriano
  * @version 1.0
  * 
- * Assignment: #3
- * Course: ADEV-1001 (OPEN) Programming (Java 2)
- * Section: Online
  * Date Created: 03.12.2016
  * Last Update: 03.23.2016
+ *
 **/
 
 import java.awt.*; 
